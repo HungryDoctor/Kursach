bda9cb0f416e6307fb72729e4b13b2f571cdb4fa
HungryDoctor <assassin_dok@yahoo.com>
2014-04-08 01:20:01 +0300

    Radiobuttons

--- Kursach2(WF)/Form1.cs
+++ Kursach2(WF)/Form1.cs
@@ -16,7 +16,7 @@ namespace Kursach2_WF_
         {
             InitializeComponent();
 
- 
+
         }
 
         private void textBox2_TextChanged(object sender, EventArgs e)
@@ -33,25 +33,53 @@ namespace Kursach2_WF_
         {
             int N = Convert.ToInt32(textBox1.Text);
             int M = Convert.ToInt32(textBox2.Text);
-            dataGridView1.Size = new System.Drawing.Size(N * 100, M * 100);
+            dataGridView1.Size = new System.Drawing.Size(N * 26, M * 21);
 
             dataGridView1.RowCount = N;
             dataGridView1.ColumnCount = M;
-            int i, j;
-
-            int[,] a = new int[N, M];
-            Random r = new Random(10);
+            int i = 0, j = 0;
 
             for (i = 0; i < N; i++)
             {
                 for (j = 0; j < M; j++)
                 {
-                    a[i, j] = r.Next(10);
-                    dataGridView1.Rows[i].Cells[j].Value = a[i, j].ToString();
+                    dataGridView1.Rows[i].Cells[j].Value = 0;
+                    dataGridView1.Columns[i].Width = 25;
+                    dataGridView1.Rows[i].Height = 20;
                 }
 
             }
+            //   dataGridView1.Location = new Point(this.dataGridView1.Location.X, 200);
+            //   dataGridView1.Location = new Point(this.dataGridView1.Location.Y, 200);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
 
         }
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            label2.Visible = true;
+            label3.Visible = false;
+            textBox3.Visible = false;
+            label2.Text = "T";
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            label2.Visible = true;
+            label3.Visible = false;
+            textBox3.Visible = false;
+            label2.Text = "-1";
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            label2.Visible = false;
+            label3.Visible = true;
+            textBox3.Visible = true;
+        }
+
     }
 }
