27c87cba9c092a056bd9a472d3a85b35ce2a59a2
HungryDoctor <assassin_dok@yahoo.com>
2014-04-13 23:19:35 +0300

    Matrix1  to array, input check

--- Kursach2(WF)/Form1.cs
+++ Kursach2(WF)/Form1.cs
@@ -12,10 +12,17 @@ namespace Kursach2_WF_
 {
     public partial class Form1 : Form
     {
+        int opt = 0;
+        int M;
+        int N;
         public Form1()
         {
             InitializeComponent();
 
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
 
         }
 
@@ -41,11 +48,41 @@ namespace Kursach2_WF_
                 e.Handled = true;
             }
         }
+        private void dataGridView1_EditingControlShowing(object sender, DataGridViewEditingControlShowingEventArgs e)
+        {
+            TextBox tb = e.Control as TextBox;
+            if (tb != null)
+            {
+                e.Control.KeyPress += new KeyPressEventHandler(dataGridView1_KeyPress);
+            }
+        }
+        private void dataGridView1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && e.KeyChar != '-')
+            {
+                e.Handled = true;
+            }
+
+            if (e.KeyChar == '-' && (sender as TextBox).Text.IndexOf('-') > -1)
+            {
+                e.Handled = true;
+            }
+
+            if (e.KeyChar == '-' && (sender as TextBox).Text.Length > 0)
+            {
+                e.Handled = true;
+            }
+
+        }
+
+
+
 
         private void button1_Click(object sender, EventArgs e)
         {
-            int N = Convert.ToInt32(textBox1.Text);
-            int M = Convert.ToInt32(textBox2.Text);
+            N = Convert.ToInt32(textBox1.Text);
+            M = Convert.ToInt32(textBox2.Text);
 
             if ((Convert.ToInt32(textBox1.Text) > 0) && (Convert.ToInt32(textBox2.Text) > 0))
             {
@@ -80,6 +117,7 @@ namespace Kursach2_WF_
                 radioButton1.Enabled = true;
                 radioButton2.Enabled = true;
                 radioButton3.Enabled = true;
+                button2.Enabled = true;
             }
             else
             {
@@ -87,15 +125,12 @@ namespace Kursach2_WF_
             }
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
         private void radioButton1_CheckedChanged(object sender, EventArgs e)
         {
             label2.Visible = true;
             label3.Visible = false;
             textBox3.Visible = false;
+            opt = 1;
 
             label2.Location = new Point(this.label2.Location.X, dataGridView1.Location.X + dataGridView1.Size.Width + 5);
             label2.Location = new Point(this.label2.Location.Y, dataGridView1.Location.Y - 10);
@@ -107,6 +142,7 @@ namespace Kursach2_WF_
             label2.Visible = true;
             label3.Visible = false;
             textBox3.Visible = false;
+            opt = 2;
 
             label2.Location = new Point(this.label2.Location.X, dataGridView1.Location.X + dataGridView1.Size.Width + 5);
             label2.Location = new Point(this.label2.Location.Y, dataGridView1.Location.Y - 10);
@@ -118,12 +154,68 @@ namespace Kursach2_WF_
             label2.Visible = false;
             label3.Visible = true;
             textBox3.Visible = true;
+            opt = 3;
 
             label3.Location = new Point(this.label3.Location.X, dataGridView1.Location.X - label3.Size.Width - 3);
             label3.Location = new Point(this.label3.Location.Y, dataGridView1.Location.Y + (dataGridView1.Size.Height / 2) - 7);
             textBox3.Location = new Point(this.textBox3.Location.X, dataGridView1.Location.X - textBox3.Size.Width - label3.Size.Width - 6);
             textBox3.Location = new Point(this.textBox3.Location.Y, dataGridView1.Location.Y + (dataGridView1.Size.Height / 2) - 10);
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            bool notfin = false;
+            int[,] values = new int[M, N];
+            for (int i = 0; i < dataGridView1.Columns.Count; i++)
+            {
+                for (int j = 0; j < dataGridView1.Rows.Count; j++)
+                {
+                    if (dataGridView1.Rows[j].Cells[i].Value == null)
+                    {
+                        notfin = true;
+                        break;
+                    }
+                    else
+                    {
+                        values[i, j] = Convert.ToInt32(dataGridView1.Rows[j].Cells[i].Value.ToString());
+                    }
+                }
+                if (notfin == true)
+                {
+                    MessageBox.Show("Заполните матрицу");
+                    break;
+                }
+            }
+            utils.calculate(opt, values);
+        }
+
+    }
+
+
+
+    public class utils
+    {
+
+        public static void calculate(int opt, int[,] values)
+        {
+            if (opt == 1)
+            {
+
+
+
+            }
+            else if (opt == 2)
+            {
+
+            }
+            else if (opt == 3)
+            {
+
+            }
+
+        }
+
+
 
     }
 }
